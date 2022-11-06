df153241110182b40a56ce80a36417e8e7e4d366
Kabir Atoyebi <katoyebir@gmail.com>
2022-11-06 08:00:43 -0800

    0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,6 +9,9 @@ void bubble_sort(int *array, size_t size)
 {
 	int i, j, temp;
 
+	if ((sizeof(array) / sizeof(int)) < 3)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
